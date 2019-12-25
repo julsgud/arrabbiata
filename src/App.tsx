f7beadd6621385ffa7d6565b8703e127ca45e710
Julio Gudino <julsgud@gmail.com>
2019-12-25 14:25:51 -0600

    Add styled comp types.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
 import React from 'react'
+import styled from 'styled-components'
+
+const Container = styled.div`
+  width: 100%;
+  height: 100%;
+`
 
 export function App() {
-    return <div> hola </div>
+  return (
+    <Container>
+      <div> Hola </div>
+    </Container>
+  )
 }
