e2811fe82293c0dc1ebbfd743de120df5bf7bc28
Julio Gudino <julsgud@gmail.com>
2020-01-08 21:29:14 -0600

    Using direct fauna login.

--- src/App/AppWithProviders.tsx
+++ src/App/AppWithProviders.tsx
@@ -1,14 +1,46 @@
-import React from 'react'
+import ApolloClient from 'apollo-client'
+import React, { useState } from 'react'
 import { ApolloProvider } from '@apollo/react-hooks'
+import { createHttpLink } from 'apollo-link-http'
+// import { setContext } from 'apollo-link-context'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+
 import { App } from './App'
 import { Login } from '../Login/Login'
+
+let cachedClient = unknown
+
+export const getApolloClient = () => {
+  if (cachedClient) return cachedClient
+
+  const httpLink = createHttpLink({
+    uri: '/graphql',
+  })
+
+  // const authLink = setContext((_, { headers }) => {
+  //   // get the authentication token from local storage if it exists
+  //   const token = localStorage.getItem('token')
+  //   // return the headers to the context so httpLink can read them
+  //   return {
+  //     headers: {
+  //       ...headers,
+  //       authorization: token ? `Bearer ${token}` : '',
+  //     },
+  //   }
+  // })
+
+  cachedClient = new ApolloClient({
+    link: httpLink,
+    cache: new InMemoryCache(),
+  })
+
+  return cachedClient
+}
 
 export function AppWithProviders() {
   const [isSignedIn, setIsSignedIn] = useState(false)
 
-  return (
-    <ApolloProvider client={getApolloClient(isSignedIn, setIsSignedIn)}>
-      {isSignedIn ? <App /> : <Login isSignedIn={isSignedIn} setIsSignedIn={setIsSignedIn} />}
-    </ApolloProvider>
-  )
+ if (isSignedIn) return <App/>
+
+ return <Login isSignedIn={isSignedIn} setIsSignedIn={setIsSignedIn}/>
 }
---
+++ src/services/faunaDb.ts
@@ -0,0 +1,8 @@
+const faunadb = require('faunadb')
+
+const q = faunadb.query
+const client = new faunadb.Client({
+  secret: process.env.REACT_APP_FAUNADB_SERVER_SECRET,
+})
+
+export { q, client }
