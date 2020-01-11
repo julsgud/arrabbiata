75a78c6106889987422957f68e6094771816ae20
Julio Gudino <julsgud@gmail.com>
2020-01-10 18:36:50 -0600

    Pass and uuid types.

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,6 +3,8 @@ import session from 'express-session'
 const session = require('express-session')
 const graphqlHTTP = require('express-graphql')
 const { buildSchema } = require('graphql')
+import passport from 'passport';
+import User from './User';
 import uuid from 'uuid/v4';
 
 // Construct a schema, using GraphQL schema language
@@ -18,6 +20,19 @@ const root = {
     return 'Hello world!'
   },
 }
+
+
+...
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+    const users = User.getUsers();
+    const matchingUser = users.find(user => user.id === id);
+    done(null, matchingUser);
+});
 
 const SESSION_SECRECT = 'bad secret';
 
