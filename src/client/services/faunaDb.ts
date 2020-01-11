71cc5105b2ab5090169232d2a2994efb1dfe402f
Julio Gudino <julsgud@gmail.com>
2020-01-10 18:00:01 -0600

    New npm commands.

--- src/client/services/faunaDb.ts
+++ src/client/services/faunaDb.ts
@@ -1,4 +1,4 @@
-import faunadb from 'client/services/faunaDb'
+import faunadb from 'faunadb'
 
 export const client = new faunadb.Client({ secret: window.FAUNA_CLIENT_SECRET })
 export const q = faunadb.query
