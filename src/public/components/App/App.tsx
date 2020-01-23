a95bc6a409483c0a5722f713fc23a0de615db301
Julio Gudino <julsgud@gmail.com>
2020-01-22 18:32:07 -0600

    Testing local state w apollo.

--- src/public/components/App/App.tsx
+++ src/public/components/App/App.tsx
@@ -1,36 +1,51 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 import { ApolloProvider } from '@apollo/react-hooks'
 import { createHttpLink } from 'apollo-link-http'
 import { ApolloClient } from 'apollo-client'
-import { InMemoryCache, NormalizedCacheObject } from 'apollo-cache-inmemory'
+import { InMemoryCache } from 'apollo-cache-inmemory'
 
 import { Layout } from './Layout/Layout'
 import { Gatekeeper } from '../Gatekeeper/Gatekeeper'
+import { GET_IS_RUNNING } from '../Timer/Timer'
 
 export const link = createHttpLink({
   uri: 'http://localhost:4000/graphql',
   credentials: 'include',
 })
 
-let client: ApolloClient<NormalizedCacheObject>
-
 export function App() {
+  const [client, setClient] = useState()
+
   useEffect(() => {
     const cache = new InMemoryCache()
-    client = new ApolloClient({
+    const apolloClient = new ApolloClient({
       cache,
       link,
+      resolvers: {
+        Mutation: {
+          toggleIsRunning: (a, b, { cache }) => {
+            const previousData = cache.readQuery({ query: GET_IS_RUNNING })
+            const data = { isRunning: !previousData.isRunning }
+            cache.writeQuery({ query: GET_IS_RUNNING, data })
+          },
+        },
+      },
     })
 
     cache.writeData({
       data: {
-        isRunning: false,
-        isOnCycle: true,
-      }
+        timer: {
+          isRunning: false,
+          isOnCycle: true,
+        },
+      },
     })
 
+    setClient(apolloClient)
   }, [])
+
+  if (client === undefined) return <div>Loading...</div>
 
   return (
     <ApolloProvider client={client}>
--- src/public/components/Timer/Timer.tsx
+++ src/public/components/Timer/Timer.tsx
@@ -1,25 +1,51 @@
 import React, { useEffect, useState } from 'react'
 import { secondsToMinutesSecondsFormat } from './Timer.util'
+import gql from 'graphql-tag'
+import { useMutation, useQuery } from '@apollo/react-hooks'
 
 const TWENTY_FIVE_MINUTES_IN_SECONDS = 1500
 const FIVE_MINUTES_IN_SECONDS = 300
 
+const TOGGLE_IS_RUNNING = gql`
+  mutation ToggleIsRunning($isRunning: Boolean) {
+    toggleIsRunning(isRunning: $isRunning) @client
+  }
+`
+
+export const GET_IS_RUNNING = gql`
+  query GetTimer {
+    timer @client {
+      isRunning 
+      isOnCycle
+    }
+  }
+`
+
 export function Timer() {
   const [currentlyOnWorkCountdown, setCurrentlyOnWorkCountdown] = useState(true)
   const [currentTimeInSeconds, setCurrentTimeInSeconds] = useState(TWENTY_FIVE_MINUTES_IN_SECONDS)
-  const [isActive, setIsActive] = useState(false)
+  const {
+    data: {
+      timer: { isRunning, isOnCycle },
+    },
+  } = useQuery(GET_IS_RUNNING)
+  const [toggleIsRunning] = useMutation(TOGGLE_IS_RUNNING)
   const [workCount, setWorkCount] = useState(0)
   const [breakCount, setBreakCount] = useState(0)
 
+  console.log(toggleIsRunning)
+  console.log(isRunning)
+  console.log(isOnCycle)
+
   function toggle() {
-    setIsActive(!isActive)
+    toggleIsRunning()
   }
 
   function reset() {
     setCurrentTimeInSeconds(
       currentlyOnWorkCountdown ? TWENTY_FIVE_MINUTES_IN_SECONDS : FIVE_MINUTES_IN_SECONDS
     )
-    setIsActive(false)
+    toggleIsRunning()
   }
 
   function startBreakCountdown() {
@@ -36,18 +62,18 @@ export function Timer() {
 
   useEffect(() => {
     let interval = 0
-    if (isActive) {
+    if (isRunning) {
       if (currentTimeInSeconds === 0) {
         currentlyOnWorkCountdown ? startBreakCountdown() : startWorkCountdown()
       }
       interval = setInterval(() => {
         setCurrentTimeInSeconds(currentTimeInSeconds => currentTimeInSeconds - 1)
       }, 1000)
-    } else if (!isActive && currentTimeInSeconds !== 0) {
+    } else if (!isRunning && currentTimeInSeconds !== 0) {
       clearInterval(interval)
     }
     return () => clearInterval(interval)
-  }, [isActive, currentTimeInSeconds])
+  }, [isRunning, currentTimeInSeconds])
 
   return (
     <>
@@ -57,7 +83,7 @@ export function Timer() {
       <br />
       {'Breaks: ' + breakCount}
       <br />
-      <button onClick={toggle}>{isActive ? 'Pause' : 'Play'}</button>
+      <button onClick={toggle}>{isRunning ? 'Pause' : 'Play'}</button>
       <button onClick={reset}> Reset </button>
     </>
   )
