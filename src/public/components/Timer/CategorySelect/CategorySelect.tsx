85b37ce18b7ab3d6382091bc78243f28bed0dc14
Julio Gudino <julsgud@gmail.com>
2020-01-26 16:47:26 -0600

    Add cat to cycle.

--- src/public/components/Timer/CategorySelect/CategorySelect.tsx
+++ src/public/components/Timer/CategorySelect/CategorySelect.tsx
@@ -1,17 +1,21 @@
 import React from 'react'
+import { useSetCycleCategoryMutation } from '../../../../generated/graphql'
 export function CategorySelect({ selectedCategoryId, categories }) {
-
+  const [setCycleCategory] = useSetCycleCategoryMutation()
 
   return (
-    <select value={selectedCategoryId}>
+    <select
+      value={selectedCategoryId}
+      onChange={e => {
+        return setCycleCategory({ variables: { categoryId: e.target.value } })
+      }}
+    >
       {categories.map(cat => (
         <option key={cat.id} value={cat.id}>
           {cat.categoryName}
         </option>
       ))}
-      <option value="free">
-        Free
-      </option>
+      <option value="free">Free</option>
     </select>
   )
 }
